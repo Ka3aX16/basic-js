867601da4447d2571ffa794066f408b47030d18e
Ka3aX16 <77680201+Ka3aX16@users.noreply.github.com>
2021-10-05 05:41:34 +0600

    fix: values

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,7 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
+export default function repeater(str, options) {
 	if (!options.hasOwnProperty('separator')) {
 		options.separator = '+';
 	 }
@@ -25,10 +25,10 @@ export default function repeater(/* str, options */) {
 	 if (options.hasOwnProperty('addition') && options.addition === null) {
 			options.addition = 'null';
 	 }
-	 let addArr = new Array(options.additionRepeatTimes).fill(options.addition);
-	 let addStr = addArr.join(options.additionSeparator);
-	 let partStr = str + addStr;
-	 let resultArr = new Array(options.repeatTimes).fill(partStr);
+	 let addArray = new Array(options.additionRepeatTimes).fill(options.addition);
+	 let addString = addArr.join(options.additionSeparator);
+	 let partStrting = str + addStr;
+	 let resultArray = new Array(options.repeatTimes).fill(partStr);
 	 let result = resultArr.join(options.separator);
   return result;
 };
