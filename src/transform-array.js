7db100e3ef348702e08d565a9337f45ffc3951b8
Ka3aX16 <77680201+Ka3aX16@users.noreply.github.com>
2021-10-05 04:43:37 +0600

    feat: transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -14,6 +14,16 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+  if (!Array.isArray(arr)) throw new Error () 
+
+  let newArray=[...arr];
+  
+  for (let i=0; i<arr.length; i++) {
+    if (arr[i] === '--double-prev') newArray.splice(i,1,newArray[i-1]);
+    else if (arr[i] === '--double-next') newArray.splice(i,1,arr[i+1]);
+    else if (arr[i] === '--discard-next') newArray.splice(i+1,1,undefined);
+    else if (arr[i] === '--discard-prev' && i>0) newArray.splice(i-1,1,undefined)
+  }
+
+  return newArray.filter(el => el!==undefined && el!=='--discard-prev' && el!=='--discard-next');
+};
