b845403fb1be01decf0d14437f562df948db6d24
Ka3aX16 <77680201+Ka3aX16@users.noreply.github.com>
2021-10-05 04:47:05 +0600

    feat: what-season.js

--- src/what-season.js
+++ src/what-season.js
@@ -12,6 +12,36 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+  if (!date) {
+    return 'Unable to determine the time of year!'
+  }
+  if (Object.prototype.toString.call(date) !== '[object Date]') {
+    throw 'Error';
+  }
+  try {
+    const month = date.getMonth();
+    switch (month) {
+      case 11:
+      case 0:
+      case 1:
+        return 'winter';
+      case 2:
+      case 3:
+      case 4:
+        return 'spring';
+      case 5:
+      case 6:
+      case 7:
+        return 'summer';
+      case 8:
+      case 9:
+      case 10:
+        return 'autumn';
+      default:
+        return false;
+    }
+  } catch (e) {
+    return e
+  }
+
+};
