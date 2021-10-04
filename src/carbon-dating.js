60534aa501798394096835dbb62af8ae9f28633e
Ka3aX16 <77680201+Ka3aX16@users.noreply.github.com>
2021-10-05 04:23:20 +0600

    feat: carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -18,6 +18,18 @@ const HALF_LIFE_PERIOD = 5730;
  *
  */
 export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+  //throw new CustomError('Not implemented');
   // remove line with error and write your code here
-}
+  let result;
+  if (typeof sampleActivity == "object" || typeof sampleActivity == "number") {
+      return false;
+  }
+
+  if (sampleActivity && parseFloat(sampleActivity) && (sampleActivity > 0 && sampleActivity < MODERN_ACTIVITY)) {
+      result = Math.ceil((Math.log((MODERN_ACTIVITY / sampleActivity)) / 0.693) * HALF_LIFE_PERIOD);
+      return result;
+  } else {
+      return false;
+  }
+
+};
