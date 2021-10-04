43e812137aa56c259a2b5acaf3a078bed459413f
Ka3aX16 <77680201+Ka3aX16@users.noreply.github.com>
2021-10-05 05:48:40 +0600

    fix: values

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,6 +13,17 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
+export default function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error ("'arr' parameter must be an instance of the Array!") 
+
+  let newArr = [...arr];
   
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '--double-prev') newArr.splice(i,1,newArr[i-1]);
+    else if (arr[i] === '--double-next') newArr.splice(i,1,arr[i+1]);
+    else if (arr[i] === '--discard-next') newArr.splice(i+1,1,undefined);
+    else if (arr[i] === '--discard-prev' && i>0) newArr.splice(i-1,1,undefined)
+  }
+
+  return newArr.filter(el => el!==undefined && el!=='--discard-prev' && el!=='--discard-next');
 }
