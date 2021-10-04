46047f7c1946a34b7e62a5288c63d623dd6a1d72
Ka3aX16 <77680201+Ka3aX16@users.noreply.github.com>
2021-10-05 04:27:26 +0600

    feat: extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -16,6 +16,19 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  */
 export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+	if (!options.hasOwnProperty('separator')) {
+		options.separator = '+';
+	 }
+	 if (!options.hasOwnProperty('additionSeparator') && options.hasOwnProperty('additionRepeatTimes')) {
+		options.additionSeparator = '|';
+	 }
+	 if (options.hasOwnProperty('addition') && options.addition === null) {
+			options.addition = 'null';
+	 }
+	 let addArr = new Array(options.additionRepeatTimes).fill(options.addition);
+	 let addStr = addArr.join(options.additionSeparator);
+	 let partStr = str + addStr;
+	 let resultArr = new Array(options.repeatTimes).fill(partStr);
+	 let result = resultArr.join(options.separator);
+  return result;
+};
